e2cef2ccad910a15248f82671d95bab638eca251
liuyang <liuyang08035@gmail.com>
2024-03-20 22:55:44 +0800

    using mysql

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,12 +85,24 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoDB',
+        'USER': 'root',
+        'PASSWORD': '08035LIUYANGl',
+        'HOST': 'localhost',  
+        'PORT': '3306',      
     }
 }
+
 
 
 # Password validation
