346ba2540d9db1c83d9d3441cb544e09a39bf79f
liuyang <liuyang08035@gmail.com>
2024-05-18 19:18:45 +0800

    add onnx_infer && send_ng

--- camera/camera_process_with_ws_model.py
+++ camera/camera_process_with_ws_model.py
@@ -18,6 +18,8 @@ from asgiref.sync import sync_to_async
 from concurrent.futures import ThreadPoolExecutor
 import time
 
+from .onnx_infer import YOLOv8
+
 sys.path.insert(0, "..")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
@@ -25,11 +27,27 @@ django.setup()
 
 from photologue.models import Photo, Gallery
 import logging
+from plc.plc_control import plcControl
+from django.conf import settings
 
 logger = logging.getLogger("django")
 
 current_dir = Path(__file__).resolve().parent
 configure_dir = current_dir / 'configure'
+
+onnx_path = settings.onnx_path
+conf_thres = settings.conf_thres
+iou_thres = settings.iou_thres
+ng_ids = settings.ng_ids
+camera_num = settings.camera_num
+detection = YOLOv8(onnx_path, conf_thres, iou_thres)
+
+plc = plcControl()
+if not plc.connect():
+    logger.info("~~~~~~~~~~~~~~~~~~~~ ng signal camera process ~~~~~~~~~~~~~~~~~~~~")
+    logger.info("plc is offline")
+    logger.info("~~~~~~~~~~~~~~~~~~~~ ng signal camera process ~~~~~~~~~~~~~~~~~~~~")
+    plc = None
 
 
 def get_random_image(text):
@@ -88,9 +106,23 @@ def prepare_one_image(img_io, gallery_title, camera_info, ng=False):
 
 
 def get_AI_results(img_io):
-    rand_int = np.random.uniform(0, 1)
-    ng = False if rand_int < 0.999 else True
-    return ng
+    pred_img, pred_ids = detection.process_one_image(img_io)
+    pred_ids_s = set(pred_ids)
+    pred_ng_ids = pred_ids_s.intersection(ng_ids)
+    ng = True if len(pred_ng_ids) else False
+
+    frame = Image.fromarray(pred_img)
+    buffer = io.BytesIO()
+    frame.save(buffer, format="JPEG")
+    return ng, buffer
+
+def send_ng(gallery_id):
+    _id = int(gallery_id.split("_")[-1])
+    if plc is not None:
+        if camera_num != 4:
+            _id = _id // 3
+        plc.set_M(f"M_1{_id + 1}")
+        logger.info(f"----------send ng: M_1{_id + 1}===============")
 
 
 def capture_and_upload(camera_manager: cameraManager, gallery_title, plc_timestamp, delay=0.02):
@@ -107,7 +139,10 @@ def capture_and_upload(camera_manager: cameraManager, gallery_title, plc_timesta
             img_io = get_random_image(gallery_title)
             print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
     finish_timestamp = time.time()
-    ng = get_AI_results(img_io)
+    ng, img_io = get_AI_results(img_io)
+    if ng:
+        send_ng(gallery_title)
+
     camera_roi_info = camera_manager.get_camera_roi()
     camera_roi_info = dict(zip(['roi0', 'roi1', 'roi0_disabled', 'roi1_disabled'],
                                camera_roi_info))
